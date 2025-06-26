e0658ed8d58d3c62f5eec24ac07475b985934b1a
agni-prjkt 52 <agnikernel0@gmail.com>
2025-06-26 01:36:06 +0000

    KernelSU: Integrated  Manual Hook k4.14
    https://kernelsu.org/guide/how-to-integrate-for-non-gki.html#manually-modify-the-kernel-source
    
    * [backport] fs: path_umount
    
    * [security] selinux: Allow init exec ksud under nosuid
    
    Signed-off-by: agni-prjkt 52 <agnikernel0@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1739,6 +1739,7 @@ static inline bool may_mandlock(void)
 }
 #endif
 
+#ifdef CONFIG_KSU
 static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
@@ -1772,6 +1773,7 @@ int path_umount(struct path *path, int flags)
 	mntput_no_expire(mnt);
 	return ret;
 }
+#endif
 
 /*
  * Now umount can handle mount points as well as block devices.
